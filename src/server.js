b637d4d91c36db66a19ff8adf81c1f648e6cc818
gonzalo <equipostocker@gmail.com>
2020-07-13 17:51:07 -0300

    sext commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/app-galicia'));
@@ -6,4 +6,13 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/app-galicia/index.html'));
 });
+app.listen(process.env.PORT || 8080);*/
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
